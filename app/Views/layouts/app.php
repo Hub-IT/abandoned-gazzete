01b59c2bd64637ee46ea6f017cfde0106e3cf072
Antony <anthonykalogeropoulos@gmail.com>
2015-06-11 23:27:37 +0300

    Add bower scripts in composer.json

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -5,14 +5,14 @@
 	<title>AdminLTE 2 | Dashboard</title>
 	<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 	<!-- Bootstrap 3.3.4 -->
-	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+	<link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
 	<!-- Font Awesome Icons -->
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"
 	      type="text/css"/>
 	<!-- Ionicons -->
 	<link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css"/>
 	<!-- Theme style -->
-	<link href="vendor/admin-lte/css/AdminLTE.min.css" rel="stylesheet" type="text/css"/>
+	<link href="../vendor/admin-lte/css/AdminLTE.min.css" rel="stylesheet" type="text/css"/>
 	<!-- AdminLTE Skins. Choose a skin from the css/skins
 		 folder instead of downloading all of them to reduce the load. -->
 	<link href="vendor/admin-lte/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css"/>
