593c724b16b528cf875bacfe339c24d3f87bbbd8
Rizart Dokollari <r.dokollari@gmail.com>
2015-06-21 09:50:02 +0300

    Construct Hmlt::anchor helper.

--- app/Helpers/Html.php
+++ app/Helpers/Html.php
@@ -6,26 +6,31 @@ use Gazzete\Kernel\App;
  * @author  Rizart Dokollari
  * @version 6/16/2015
  */
-class Html {
+class Html
+{
 
-	public function link($url, $rel = 'stylesheet', $type = 'text/css')
-	{
-		return "<link href='" . App::getBaseURL() . "/$url' rel='$rel' type='$type' />";
-	}
+    public function link($url, $rel = 'stylesheet', $type = 'text/css')
+    {
+        return "<link href='" . App::getBaseURL() . "/$url' rel='$rel' type='$type' />";
+    }
 
-	public function script($src)
-	{
-		return "<script src='" . App::getBaseURL() . "/$src'></script>";
-	}
+    public function script($src)
+    {
+        return "<script src='" . App::getBaseURL() . "/$src'></script>";
+    }
 
-	public function documentWriteScript($src)
-	{
-		return "document.write(\"<script src='" . App::getBaseURL() . "/$src'><\\/script>\")";
-	}
+    public function documentWriteScript($src)
+    {
+        return "document.write(\"<script src='" . App::getBaseURL() . "/$src'><\\/script>\")";
+    }
 
-// TODO: write unit test
-	public function url($url)
-	{
-		return App::getBaseURL() . "/$url";
-	}
+    public function url($url)
+    {
+        return App::getBaseURL() . "/$url";
+    }
+
+    public function anchor($href, $text, $id = NULL, $class = NULL)
+    {
+        return "<a href='$href' class='$class' id='$id' >$text</a>";
+    }
 }
--- tests/unit/app/Helpers/HtmlTest.php
+++ tests/unit/app/Helpers/HtmlTest.php
@@ -1,53 +1,71 @@
 <?php namespace app\Helpers;
 
+use Faker\Factory;
 use Gazzete\Helpers\Html;
 use Gazzete\Kernel\App;
 
-class HtmlTest extends \PHPUnit_Framework_TestCase {
+class HtmlTest extends \PHPUnit_Framework_TestCase
+{
 
-	private $html;
+    private $html;
 
-	protected function setUp()
-	{
-		$this->html = new Html();
+    private $faker;
+
+    protected function setUp()
+    {
+        $this->html = new Html();
+
+        $this->faker = Factory::create();
 	}
 
-	protected function tearDown()
-	{
-	}
+    protected function tearDown()
+    {
+    }
 
-	public function testReturnsCorrectLink()
-	{
-		$url = App::getBaseURL() . "/asset.css";
+    public function testReturnsCorrectLink()
+    {
+        $url = App::getBaseURL() . "/asset.css";
 
-		$expected = "<link href='$url' rel='stylesheet' type='text/css' />";
+        $expected = "<link href='$url' rel='stylesheet' type='text/css' />";
 
-		$this->assertEquals($expected, $this->html->link('asset.css'));
-	}
+        $this->assertEquals($expected, $this->html->link('asset.css'));
+    }
 
-	public function testReturnsCorrectScript()
-	{
-		$url = App::getBaseURL() . "/asset.js";
+    public function testReturnsCorrectScript()
+    {
+        $url = App::getBaseURL() . "/asset.js";
 
-		$expected = "<script src='$url'></script>";
+        $expected = "<script src='$url'></script>";
 
-		$this->assertEquals($expected, $this->html->script('asset.js'));
-	}
+        $this->assertEquals($expected, $this->html->script('asset.js'));
+    }
 
-	public function testReturnsCorrectDocumentScript()
-	{
-		$url = App::getBaseURL() . "/asset.js";
+    public function testReturnsCorrectDocumentScript()
+    {
+        $url = App::getBaseURL() . "/asset.js";
 
-		$expected = 'document.write("<script src=\'' . $url . '\'><\\/script>")';
+        $expected = 'document.write("<script src=\'' . $url . '\'><\\/script>")';
 
-		$this->assertEquals($expected, $this->html->documentWriteScript('asset.js'));
-	}
+        $this->assertEquals($expected, $this->html->documentWriteScript('asset.js'));
+    }
 
-	public function testReturnsCorrectUrl()
-	{
-		$expected = App::getBaseURL() . "/url";
+    public function testReturnsCorrectUrl()
+    {
+        $expected = App::getBaseURL() . "/url";
 
-		$this->assertEquals($expected, $this->html->url('url'));
-	}
+        $this->assertEquals($expected, $this->html->url('url'));
+    }
+
+    public function testReturnsCorrectWithARelativeAnchor()
+    {
+        $href = App::getBaseURL() . "/url";
+        $text = $this->faker->paragraph;
+        $class = $this->faker->optional(0.5, null)->word();
+        $id = $this->faker->optional(0.5, null)->word();
+
+        $expected = "<a href='$href' class='$class' id='$id' >$text</a>";
+
+        $this->assertEquals($expected, $this->html->anchor($href, $text, $id, $class));
+    }
 
 }
